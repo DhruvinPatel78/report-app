2bbdc75972d1e5071266550782dbadbda3a93492
DhruvinPatel78 <patel.dhruvinpatel@gmail.com>
2025-04-07 14:37:31 +0530

    fix changes

--- components/DraggableCard.tsx
+++ components/DraggableCard.tsx
@@ -1,7 +1,21 @@
+import React from "react";
 import { Card } from "@/components/ui/card";
 import { GripVertical } from "lucide-react";
 
-const DraggableCard = ({
+interface DraggableCardProps {
+  card: {
+    id: number;
+    title: string;
+  };
+  onDragStart: (e: React.DragEvent<HTMLDivElement>, id: number) => void;
+  onDragOver: (e: React.DragEvent<HTMLDivElement>) => void;
+  onDrop: (e: React.DragEvent<HTMLDivElement>, id: number) => void;
+  currentId?: string;
+  onClick: (id: number, index: number) => void;
+  index: number;
+}
+
+const DraggableCard: React.FC<DraggableCardProps> = ({
   card,
   onDragStart,
   onDragOver,
@@ -20,7 +34,7 @@ const DraggableCard = ({
       className={`p-4 ${currentId?.toString() === card.id.toString() ? "bg-[#ebebeb] font-bold" : "bg-white"} shadow-none cursor-pointer flex flex-row gap-2 rounded-none border-b border-gray-200 `}
     >
       <GripVertical
-        className={` ${currentId?.toString() === card.id.toString() ? "text-black" : "text-[#747474]"} `}
+        className={`${currentId?.toString() === card.id.toString() ? "text-black" : "text-[#747474]"}`}
       />
       <span className={"truncate"}>{card.title}</span>
     </Card>
